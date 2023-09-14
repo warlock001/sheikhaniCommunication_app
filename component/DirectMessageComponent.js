94aaa3a5ef550b1ebd67370d1a55557fa1cd1b57
Zabloon <zabloona@gmail.com>
2023-09-14 08:43:36 +0500

    added

--- component/DirectMessageComponent.js
+++ component/DirectMessageComponent.js
@@ -27,189 +27,189 @@ export default function DirectMessageComponent({
 
     getStatus();
   }, []);
-  getStatus()
-}, [])
 
 
-useLayoutEffect(() => {
-  async function getImage() {
-    await axios
-      .get(`http://192.168.0.103:3001/user?id=${item.senderid}`)
-      .then(async result => {
-        console.log('image ->', result.data.user.profilePicture[0]);
-        await axios
-          .get(
-            `http://192.168.0.103:3001/files/${result.data.user.profilePicture[0]}/true`,
-          )
-          .then(image => {
-            setImage(
-              `data:${image.headers['content-type']};base64,${image.data}`,
-            );
-          });
-      })
-      .catch(err => {
-        console.log('err', err);
-      });
-  }
-  getImage()
-}, [])
 
-useLayoutEffect(() => {
-  async function loadImage() {
-    if (item.isPicture) {
-      await axios.get(`http://192.168.0.103:3001/files/${item.message}/true`).then(result => {
-        setMediaImage(
-          `data:${result.headers['content-type']};base64,${result.data}`,
-        );
-      })
-    }
-  }
-  loadImage()
-}, [])
-
-const hour =
-  date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`;
-
-const mins =
-  date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
-
-return (
-  <TouchableOpacity
-    onLongPress={() => {
-      setDelivered(item.createdAt);
-      setSeen(item.updatedAt);
-      setReceiptsModalVisible(true);
-    }}>
-    <View key={item._id}>
-      <View
-        style={
-          status
-            ? [styles.mmessageWrapper]
-            : [styles.mmessageWrapper, { alignItems: 'flex-end' }]
-        }>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          {status ? (
-            image ? (
-              <Image
-                resizeMode="cover"
-                style={{
-                  width: 30,
-                  height: 30,
-                  borderWidth: 0.5,
-                  borderColor: '#ddd',
-                  borderRadius: 100,
-                  marginRight: 2,
-                  marginTop: 'auto',
-                }}
-                source={{ uri: image }}
-              />
-            ) : (
-              // <Text>{image}</Text>
-              <Image
-                resizeMode="cover"
-                style={{
-                  width: 30,
-                  height: 30,
-                  marginTop: 'auto',
-                }}
-                source={require('../images/myaccount2.png')}
-              />
+  useLayoutEffect(() => {
+    async function getImage() {
+      await axios
+        .get(`http://192.168.0.103:3001/user?id=${item.senderid}`)
+        .then(async result => {
+          console.log('image ->', result.data.user.profilePicture[0]);
+          await axios
+            .get(
+              `http://192.168.0.103:3001/files/${result.data.user.profilePicture[0]}/true`,
             )
-          ) : (
-            ''
-          )}
+            .then(image => {
+              setImage(
+                `data:${image.headers['content-type']};base64,${image.data}`,
+              );
+            });
+        })
+        .catch(err => {
+          console.log('err', err);
+        });
+    }
+    getImage()
+  }, [])
 
-          {
-            item.isPicture
-              ?
-              <Image
-                resizeMode="cover"
-                // style={[styles.mavatar, { marginTop: 'auto' }]}
-                source={{ uri: mediaImage }}
-                width={300}
-                height={300}
-              />
-              :
-              <View
-                style={
-                  status
-                    ? [styles.mmessage, { borderBottomRightRadius: 10 }]
-                    : [
-                      styles.mmessage,
-                      { backgroundColor: "#1F2067", borderBottomLeftRadius: 10 },
-                    ]
-                }
-              >
-                {(status && item.title) ? <Text style={{ color: '#1F2067', marginBottom: 5, fontWeight: '500', fontSize: 14 }}>{item.title}</Text> : ''}
-                <Text style={status ? [{ color: "#000" }] : [{ color: "#FFF" }]}>
-                  {
+  useLayoutEffect(() => {
+    async function loadImage() {
+      if (item.isPicture) {
+        await axios.get(`http://192.168.0.103:3001/files/${item.message}/true`).then(result => {
+          setMediaImage(
+            `data:${result.headers['content-type']};base64,${result.data}`,
+          );
+        })
+      }
+    }
+    loadImage()
+  }, [])
 
-                    item.message
-                  }
-                </Text>
-                <Text
+  const hour =
+    date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`;
+
+  const mins =
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
+
+  return (
+    <TouchableOpacity
+      onLongPress={() => {
+        setDelivered(item.createdAt);
+        setSeen(item.updatedAt);
+        setReceiptsModalVisible(true);
+      }}>
+      <View key={item._id}>
+        <View
+          style={
+            status
+              ? [styles.mmessageWrapper]
+              : [styles.mmessageWrapper, { alignItems: 'flex-end' }]
+          }>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            {status ? (
+              image ? (
+                <Image
+                  resizeMode="cover"
+                  style={{
+                    width: 30,
+                    height: 30,
+                    borderWidth: 0.5,
+                    borderColor: '#ddd',
+                    borderRadius: 100,
+                    marginRight: 2,
+                    marginTop: 'auto',
+                  }}
+                  source={{ uri: image }}
+                />
+              ) : (
+                // <Text>{image}</Text>
+                <Image
+                  resizeMode="cover"
+                  style={{
+                    width: 30,
+                    height: 30,
+                    marginTop: 'auto',
+                  }}
+                  source={require('../images/myaccount2.png')}
+                />
+              )
+            ) : (
+              ''
+            )}
+
+            {
+              item.isPicture
+                ?
+                <Image
+                  resizeMode="cover"
+                  // style={[styles.mavatar, { marginTop: 'auto' }]}
+                  source={{ uri: mediaImage }}
+                  width={300}
+                  height={300}
+                />
+                :
+                <View
                   style={
                     status
-                      ? [
-                        {
-                          position: "absolute",
-                          bottom: 0,
-                          right: 7,
-                          fontSize: 10,
-                          fontWeight: "400",
-                          color: "#1F2067",
-                        },
-                      ]
+                      ? [styles.mmessage, { borderBottomRightRadius: 10 }]
                       : [
-                        {
-                          position: "absolute",
-                          bottom: 0,
-                          right: 7,
-                          fontSize: 10,
-                          fontWeight: "400",
-                          color: "#fff",
-                        },
+                        styles.mmessage,
+                        { backgroundColor: "#1F2067", borderBottomLeftRadius: 10 },
                       ]
                   }
                 >
-                  {hour + ":" + mins}
-                </Text>
-              </View>
-          }
+                  {(status && item.title) ? <Text style={{ color: '#1F2067', marginBottom: 5, fontWeight: '500', fontSize: 14 }}>{item.title}</Text> : ''}
+                  <Text style={status ? [{ color: "#000" }] : [{ color: "#FFF" }]}>
+                    {
 
-          {status ? (
-            ''
-          ) : (
-            <View
-              style={{
-                marginTop: 'auto',
-                alignItems: 'center',
-                marginHorizontal: 3,
-              }}>
-              {item.seen ? (
-                <Image
-                  resizeMode="contain"
-                  style={{
-                    display: 'flex',
-                    marginTop: 'auto',
-                  }}
-                  source={require('../images/seen.png')}
-                />
-              ) : (
-                <Image
-                  resizeMode="contain"
-                  style={{
-                    display: 'flex',
-                    marginTop: 'auto',
-                  }}
-                  source={require('../images/delivered.png')}
-                />
-                }
-            </View>
-          )}
+                      item.message
+                    }
+                  </Text>
+                  <Text
+                    style={
+                      status
+                        ? [
+                          {
+                            position: "absolute",
+                            bottom: 0,
+                            right: 7,
+                            fontSize: 10,
+                            fontWeight: "400",
+                            color: "#1F2067",
+                          },
+                        ]
+                        : [
+                          {
+                            position: "absolute",
+                            bottom: 0,
+                            right: 7,
+                            fontSize: 10,
+                            fontWeight: "400",
+                            color: "#fff",
+                          },
+                        ]
+                    }
+                  >
+                    {hour + ":" + mins}
+                  </Text>
+                </View>
+            }
+
+            {status ? (
+              ''
+            ) : (
+              <View
+                style={{
+                  marginTop: 'auto',
+                  alignItems: 'center',
+                  marginHorizontal: 3,
+                }}>
+                {item.seen ? (
+                  <Image
+                    resizeMode="contain"
+                    style={{
+                      display: 'flex',
+                      marginTop: 'auto',
+                    }}
+                    source={require('../images/seen.png')}
+                  />
+                ) : (
+                  <Image
+                    resizeMode="contain"
+                    style={{
+                      display: 'flex',
+                      marginTop: 'auto',
+                    }}
+                    source={require('../images/delivered.png')}
+                  />
+
+                )}
+              </View>
+            )}
+          </View>
         </View>
       </View>
-    </View>
-  </TouchableOpacity>
-);
+    </TouchableOpacity>
+  );
 }
